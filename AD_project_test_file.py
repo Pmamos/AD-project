4c48a79ddd557d05fd007f040532b4c0e6aa3940
Pmamos <piotrrafalmamos@gmail.com>
2024-05-08 22:54:02 +0200

    init

--- AD_project_test_file.py
+++ AD_project_test_file.py
@@ -4,11 +4,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # Uruchom Chrome w trybie headless
+chrome_options.add_argument("--headless")
 
-driver = webdriver.Chrome(options=chrome_options)  # Użyj ścieżki do sterownika, jeśli nie jest on zainstalowany w PATH
+driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://www.worldometers.info/world-population/')
-time.sleep(2)  # Czekaj na załadowanie strony
+time.sleep(2)
 def get_population():
     population = driver.find_element(By.CSS_SELECTOR,'#maincounter-wrap > div > span').text
     return population
